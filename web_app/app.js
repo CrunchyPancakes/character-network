ac2ab6563fd9c367afb182ff066dc9dc51de996f
CrunchyPancakes <30matthewt@gmail.com>
2019-12-26 18:31:33 +1300

    Can now edit items in the list.

--- web_app/app.js
+++ web_app/app.js
@@ -22,19 +22,76 @@ class UI {
     const row = document.createElement("tr");
 
     row.innerHTML = `
+        <td>${char.id}</td>
         <td>${char.firstName}</td>
         <td>${char.lastName}</td>
         <td>${char.clan}</td>
-        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        <td>
+          <a href="#" class="btn btn-danger btn-sm delete">X</a>
+          <a href="#" class="btn btn-info btn-sm edit">Edit</a>
+        </td>
     `;
 
     list.appendChild(row);
   }
 
   static deleteChar(el) {
-    if (el.classList.contains("delete")) {
-      el.parentElement.parentElement.remove();
-    }
+    el.parentElement.parentElement.remove();
+  }
+
+  // Open editing fields in a row
+  static openEdit(el, id) {
+    const charToBeEdited = Store.getChar(id);
+
+    el.parentElement.parentElement.innerHTML = `
+      <td>${charToBeEdited.id}</td>
+      <form id = "char-edit-form">
+      <td><input type="text" id="edit-first" class="form-control" value=${charToBeEdited.firstName} /></td>
+      <td><input type="text" id="edit-last" class="form-control" value=${charToBeEdited.lastName} /></td>
+      <td><input type="text" id="edit-clan" class="form-control" value=${charToBeEdited.clan} /></td>
+      <td>
+        <a href="#" class="btn btn-info btn-sm save-edit">Save</a>
+        <a href="#" class="btn btn-danger btn-sm cancel-edit">Cancel</a>
+      </td>
+      </form>
+    `;
+  }
+
+  static cancelEdit(el, id) {
+    const charToBeEdited = Store.getChar(id);
+
+    el.parentElement.parentElement.innerHTML = `
+    <td>${charToBeEdited.id}</td>
+    <td>${charToBeEdited.firstName}</td>
+    <td>${charToBeEdited.lastName}</td>
+    <td>${charToBeEdited.clan}</td>
+    <td>
+      <a href="#" class="btn btn-danger btn-sm delete">X</a>
+      <a href="#" class="btn btn-info btn-sm edit">Edit</a>
+    </td>
+    `;
+  }
+
+  static saveEditToList(el, id) {
+    const charToBeEdited = Store.getChar(id);
+
+    const first = document.querySelector("#edit-first").value;
+    const last = document.querySelector("#edit-last").value;
+    const clan = document.querySelector("#edit-clan").value;
+
+    el.parentElement.parentElement.innerHTML = `
+    <td>${charToBeEdited.id}</td>
+    <td>${first}</td>
+    <td>${last}</td>
+    <td>${clan}</td>
+    <td>
+      <a href="#" class="btn btn-danger btn-sm delete">X</a>
+      <a href="#" class="btn btn-info btn-sm edit">Edit</a>
+    </td>
+    `;
+
+    // Also save edit to storage, getting values from UI
+    Store.editChar(id, first, last, clan);
   }
 
   static showAlert(message, className) {
@@ -69,6 +126,20 @@ class Store {
     return chars;
   }
 
+  // Get a specific character by it's ID number
+  static getChar(id) {
+    const chars = Store.getChars();
+    let charToReturn;
+
+    chars.forEach(char => {
+      if (char.id === id) {
+        charToReturn = char;
+      }
+    });
+
+    return charToReturn;
+  }
+
   static addChar(char) {
     const chars = Store.getChars();
 
@@ -78,22 +149,39 @@ class Store {
   }
 
   // Edit this to remove by a unique id, rather than name
-  static removeChar(firstName) {
+  static removeChar(id) {
     const chars = Store.getChars();
 
     chars.forEach((char, index) => {
-      if (char.firstName === firstName) {
+      if (char.id === id) {
         chars.splice(index, 1);
       }
     });
 
     localStorage.setItem("chars", JSON.stringify(chars));
   }
+
+  static editChar(id, first, last, clan) {
+    let charToEdit = Store.getChar(id);
+    const chars = Store.getChars();
+
+    charToEdit.firstName = first;
+    charToEdit.lastName = last;
+    charToEdit.clan = clan;
+
+    chars.forEach((char, index) => {
+      if (char.id === id) {
+        chars.splice(index, 1, charToEdit);
+      }
+    });
+
+    localStorage.setItem("chars", JSON.stringify(chars));
+  }
 }
-// Event: Display Books
+// Event: Display Characters
 document.addEventListener("DOMContentLoaded", UI.displayChars);
 
-// Event: Add a Book
+// Event: Add a Character
 document.querySelector("#char-form").addEventListener("submit", e => {
   // Prevent actual submit
   e.preventDefault();
@@ -134,19 +222,40 @@ document.querySelector("#char-form").addEventListener("submit", e => {
   }
 });
 
-// Event: Remove a Character
+// Event: Table row button is clicked
 document.querySelector("#char-list").addEventListener("click", e => {
-  // Remove book from UI
-  UI.deleteChar(e.target);
-
-  // Remove character from store
-  Store.removeChar(
-    e.target.parentElement.parentElement.firstElementChild.textContent
-  );
-  console.log(
+  const clickedCharID = parseInt(
     e.target.parentElement.parentElement.firstElementChild.textContent
   );
 
-  // Success message
-  UI.showAlert("Character removed", "success");
+  const clickedButton = e.target;
+
+  if (e.target.classList.contains("delete")) {
+    // Delete button is clicked
+
+    // Remove book from UI
+    UI.deleteChar(clickedButton);
+
+    // Remove character from store, using ID (first element on the table).
+    // Need to use parseInt() as the ID is a string on webpage, but a number in storage.
+    Store.removeChar(clickedCharID);
+
+    // Success message
+    UI.showAlert("Character removed", "success");
+  } else if (clickedButton.classList.contains("edit")) {
+    // Edit button is clicked
+    // Open edit fields
+    UI.openEdit(clickedButton, clickedCharID);
+
+    // Update Character on UI and close edit fields
+    // Update Character in Store
+
+    // Cancel edit (closes edit fields)
+  } else if (clickedButton.classList.contains("cancel-edit")) {
+    UI.cancelEdit(clickedButton, clickedCharID);
+
+    // Save Edit
+  } else if (clickedButton.classList.contains("save-edit")) {
+    UI.saveEditToList(clickedButton, clickedCharID);
+  }
 });
